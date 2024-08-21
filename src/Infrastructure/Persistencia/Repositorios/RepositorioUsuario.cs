862249fc9cc8610ff4085ac4f777f5b661600b6d
Nayid04 <nayid2004@gmail.com>
2024-08-21 01:55:08 -0600

    Agregacion de repositorio generico y del usuario en la capa de infractructura

---
+++ src/Infrastructure/Persistencia/Repositorios/RepositorioUsuario.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistencia.Repositorios
+{
+    public class RepositorioUsuario : RepositorioGenerico<Usuario, IdUsuario>, IRepositorioUsuario
+    {
+        public RepositorioUsuario(AplicacionDeContextoDB contexto) : base(contexto)
+        {
+        }
+    }
+}
