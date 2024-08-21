d12c73fe1510cd8827887e277580f4917f0bc63b
Nayid04 <nayid2004@gmail.com>
2024-08-21 02:48:59 -0600

    Terminacion de casos de uso de los usuarios en la capa de application

---
+++ src/Application/Usuarios/Actualizar/ManejadorComandoActualizarUsuario.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Primitivos;
+using Domain.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Usuarios.Actualizar
+{
+    public sealed class ManejadorComandoActualizarUsuario : IRequestHandler<ComandoActualizarUsuario, ErrorOr<Unit>>
+    {
+        private readonly IRepositorioUsuario _repositorioUsuario;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManejadorComandoActualizarUsuario(IRepositorioUsuario repositorioUsuario, IUnitOfWork unitOfWork)
+        {
+            _repositorioUsuario = repositorioUsuario ?? throw new ArgumentNullException(nameof(repositorioUsuario));
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        public async Task<ErrorOr<Unit>> Handle(ComandoActualizarUsuario comando, CancellationToken cancellationToken)
+        {
+            if (await _repositorioUsuario.ListarPorId(new IdUsuario(comando.Id)) is not Usuario usuario)
+            {
+                return Error.NotFound("Usuario.NoEncontrado", "Usuario no encontrado.");
+            }
+
+            usuario.Actualizar(
+                comando.Nombre,
+                comando.Apellido,
+                comando.Correo
+            );
+
+            _repositorioUsuario.Actualizar(usuario);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+
+        }
+    }
+}
---
+++ src/Application/Usuarios/ListarTodos/ConsultaListarTodosLosUsuarios.cs
@@ -0,0 +1,11 @@
+﻿using Application.Usuarios.Comun;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Usuarios.ListarTodos
+{
+    public record ConsultaListarTodosLosUsuarios() : IRequest<ErrorOr<IReadOnlyList<RespuestaUsuario>>>;
+}
