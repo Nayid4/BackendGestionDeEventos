95d75ca7d9a555af8cf26074fe20179682ccf029
Nayid04 <nayid2004@gmail.com>
2024-08-21 01:31:04 -0600

    configuracion de la capa infrastructura

---
+++ src/Infrastructure/Persistencia/AplicacionDeContextoDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistencia
+{
+    public class AplicacionDeContextoDB : DbContext, IAplicacionContextoDb, IUnitOfWork
+    {
+
+
+        private readonly IPublisher _publisher;
+
+        public AplicacionDeContextoDB(DbContextOptions options, IPublisher publisher) : base(options)
+        {
+            _publisher = publisher;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AplicacionContextoDb).Assembly);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var eventosDeDominio = ChangeTracker.Entries<AggregateRoot>()
+                .Select(e => e.Entity)
+                .Where(e => e.GetDomainEvents().Any())
+                .SelectMany(e => e.GetDomainEvents());
+
+            var resultado = await base.SaveChangesAsync(cancellationToken);
+
+            foreach (var evento in eventosDeDominio)
+            {
+                await _publisher.Publish(evento, cancellationToken);
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ src/Infrastructure/Servicios/InyeccionDeDependencias.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Servicios
+{
+    public static class InyeccionDeDependencias
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection servicios, IConfiguration configuracion)
+        {
+            servicios.AgregarPersistencias(configuracion);
+            return servicios;
+        }
+
+        public static IServiceCollection AgregarPersistencias(this IServiceCollection servicios, IConfiguration configuracion)
+        {
+            servicios.AddDbContext<AplicacionContextoDb>(options =>
+                options.UseSqlServer(configuracion.GetConnectionString("Database")));
+
+            servicios.AddScoped<IAplicacionContextoDb>(sp =>
+                sp.GetRequiredService<AplicacionContextoDb>());
+
+            servicios.AddScoped<IUnitOfWork>(sp =>
+                sp.GetRequiredService<AplicacionContextoDb>());
+
+            return servicios;
+        }
+}
